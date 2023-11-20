05c782adf5dec8edc801ff1f9994afbe8280701c
TUANKIET <luctuankietkg@gmail.com>
2023-11-21 04:41:33 +0700

    Crate api get all lesson.

---
+++ src/main/java/com/poly/EasyLearning/entity/Lesson.java
@@ -0,0 +1,33 @@
+package com.poly.EasyLearning.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "lesson")
+public class Lesson {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private Boolean active = true;
+
+    @Column(length = 1024)
+    private String  title;
+
+    @Column(columnDefinition = "TEXT")
+    private String  description;
+
+    @Column(length = 1024)
+    private String  image;
+
+    @OneToMany(mappedBy = "lesson")
+    private List<Question> questions = new ArrayList<>();
+}
--- src/main/java/com/poly/EasyLearning/repository/RoleRepo.java
+++ src/main/java/com/poly/EasyLearning/repository/RoleRepo.java
@@ -4,7 +4,6 @@ import com.poly.EasyLearning.entity.RoleApp;
 import com.poly.EasyLearning.enums.RoleName;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.Optional;
 
 @Repository
